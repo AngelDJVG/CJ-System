41cd2bf1ee94598a33ef3862ed2e607c2cc28544
Ángel Valenzuela <angel.valenzuela233109@potros.itson.edu.mx>
2024-03-08 21:12:59 -0700

    Avance Capa de Presentación
    Se completó la mayoría de pantallas de manera responsiva usando layouts.

---
+++ CJCapaPresentacion/src/main/java/com/mycompany/cjcapapresentacion/CJCapaPresentacion.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cjcapapresentacion;
+
+import view.FrmComandas;
+
+/**
+ *
+ * @author Ángel Valenzuela
+ */
+public class CJCapaPresentacion {
+
+    public static void main(String[] args) {
+        new FrmComandas().setVisible(true);
+    }
+}
---
+++ CJCapaPresentacion/src/main/java/enums/TiposComanda.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package enums;
+
+/**
+ *
+ * @author Ángel Valenzuela
+ */
+public class TiposComanda {
+    final public static int PEDIDO = 0;
+    final public static int MESA = 1;
+    final public static int EXPRESS = 2;
+}
